653ceecb247b380c45af118af1bdac3e1a351968
Codacy Archive <codacy-archive@local>
2025-11-10 13:22:19 +0000

    snapshot 2025-11-10T13:22:18.954081+00:00

--- sysdeps/x86/fpu/sfp-machine.h
+++ sysdeps/x86/fpu/sfp-machine.h
@@ -1,9 +1,19 @@
 /* Configure soft-fp for building sqrtf128.  Based on sfp-machine.h in
    libgcc, with soft-float and other irrelevant parts removed.  */
 
+#include <math-inline-asm.h>
+
+#if HAVE_X86_LIBGCC_CMP_RETURN_ATTR
 /* The type of the result of a floating point comparison.  This must
    match `__libgcc_cmp_return__' in GCC for the target.  */
-typedef int __gcc_CMPtype __attribute__ ((mode (__libgcc_cmp_return__)));
+typedef int __gcc_CMPtype_GCC __attribute__ ((mode (__libgcc_cmp_return__)));
+#else
+# ifdef __x86_64__
+typedef long long int __gcc_CMPtype;
+#else
+typedef long int __gcc_CMPtype;
+# endif
+#endif
 #define CMPtype __gcc_CMPtype
 
 #ifdef __x86_64__
@@ -41,7 +51,7 @@ typedef unsigned int UTItype __attribute__ ((mode (TI)));
 
 # define FP_INIT_ROUNDMODE					\
   do {								\
-    __asm__ __volatile__ ("%vstmxcsr\t%0" : "=m" (_fcw));	\
+    stmxcsr_inline_asm (&_fcw);					\
   } while (0)
 #else
 # define _FP_W_TYPE_SIZE	32
